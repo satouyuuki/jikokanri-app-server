6220461c0a93d2399ef7130a1d3c8328d75091d8
satoyuki <yuki01377@gmail.com>
2021-03-21 00:14:35 +0900

    seed add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :targets
+  put '/targets/:id', to: 'targets#update'
+  delete '/targets/:id', to: 'targets#destroy'
+  
 end
