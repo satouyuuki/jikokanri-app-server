889ed5b0e682d821326844450b67612c38442a2a
satoyuki <yuki01377@gmail.com>
2021-04-03 22:53:20 +0900

    aws credential add

--- app/services/aws/cognitos_service.rb
+++ app/services/aws/cognitos_service.rb
@@ -3,7 +3,10 @@
 class CognitoService
   class << self
     # create client instanse 
-    COGNITO = Aws::CognitoIdentityProvider::Client.new(region: 'ap-northeast-1')
+    COGNITO = Aws::CognitoIdentityProvider::Client.new(
+      region: 'ap-northeast-1',
+      credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
+    )
 
     def authenticate(user_object)
       auth_object = {
