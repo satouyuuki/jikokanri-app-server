a4388771001fe24ea9dd6d81177c253032ca79f2
satoyuki <yuki01377@gmail.com>
2021-04-11 15:49:25 +0900

    seed delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-5.times do |no|
-  Target.create(:total_time => "#{no}0", :achieved_text => "テキスト#{no}")
-end
+# 5.times do |no|
+#   Target.create(:total_time => "#{no}0", :achieved_text => "テキスト#{no}")
+# end
